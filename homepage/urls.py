fcca82c225749dcb663b6d884623f83ffaf359c2
AymanElS4 <152307107+AymanElS4@users.noreply.github.com>
2025-08-03 02:24:32 +0000

    arreglado

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
    path("", views.index, name="index"),
-=======
+
    path("index/", views.index, name="index"),
->>>>>>> main
+
 ]
